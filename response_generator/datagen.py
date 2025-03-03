402314c1897e48ccbf36d12a255eab45534942b4
Amit Kumar <amitkh@gmail.com>
2025-03-03 10:52:49 -0500

    generate silver bullets

--- response_generator/datagen.py
+++ response_generator/datagen.py
@@ -3,19 +3,35 @@ from random import choice, randint
 import pandas as pd
 
 RESPONSES = 65
+NAMES = [choice(FIRST) + " " + choice(LAST) for  _ in range(RESPONSES)]
 
-def new_response():
+def new_response(name):
     row = []
-    row.append(choice(FIRST) + " " + choice(LAST))
+    row.append(name)
+
+    # Team preferences (random)
     for _ in SCOPE_TEAMS:
         row.append(randint(1, 5))
+
+    # add 0-2 Antipreferences (random)
+    antiprefs = []
+    for _ in range(randint(0, 2)):
+        to_add = choice(NAMES)
+        while to_add in antiprefs or to_add == name:
+            to_add = choice(NAMES)
+        antiprefs.append(to_add)
+
+    row.append(tuple(antiprefs))
+
+
+
     return row
 
 def main():
-    data = [new_response() for _ in range(RESPONSES)]
-    cols = ["Name"] + SCOPE_TEAMS
+    data = [new_response(name) for name in NAMES]
+    cols = ["Name"] + SCOPE_TEAMS + ["Antipreferences"]
     df = pd.DataFrame(data, columns=cols)
-    df.to_csv('../responses.csv', index=False)
+    df.to_json('responses.json')
 
 
 
