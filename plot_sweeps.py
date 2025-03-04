9ca48541f0dc5641cb20347ccfffb18aff1e7840
ljiang09 <ljiang@olin.edu>
2025-03-03 23:25:18 -0500

    add heuristic vs. run plot (not useful)

--- plot_sweeps.py
+++ plot_sweeps.py
@@ -16,6 +16,29 @@ def heuristic_vs_alpha(alpha, heuristic):
     plt.ylabel('Heuristic')
     plt.show()
 
+def heuristic_vs_run(alpha, runs, heuristic):
+    """
+    Line plot of the heuristic against the alpha parameter for each run
+    """
+    plt.figure()
+    for run in np.unique(runs):
+        indices = np.where(runs == run)[0]
+        
+        alpha_run = alpha[indices]
+        heuristic_run = heuristic[indices]
+        
+        sorted_indices = np.argsort(alpha_run)
+        alpha_run_sorted = alpha_run[sorted_indices]
+        heuristic_run_sorted = heuristic_run[sorted_indices]
+        
+        plt.plot(alpha_run_sorted, heuristic_run_sorted, marker='o', label=f'Run {run}')
+
+    plt.title('Heuristic vs Alpha')
+    plt.xlabel('Alpha')
+    plt.ylabel('Heuristic')
+    plt.legend(title="Run", loc="best")
+    plt.show()
+
 
 def main():
     filename = "alpha_sweep.csv"
@@ -26,6 +49,7 @@ def main():
     heuristic = data[:, 2]
 
     heuristic_vs_alpha(alpha, heuristic)
+    # heuristic_vs_run(alpha, run, heuristic)
 
 if __name__ == '__main__':
     main()
